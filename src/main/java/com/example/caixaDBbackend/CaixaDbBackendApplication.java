1dbb88b68edc05f011cf75b6f243d33a3635b7cc
ArthurVitor <arthurvitorgn@gmail.com>
2023-11-06 17:38:51 -0300

    initial commit

---
+++ src/main/java/com/example/caixaDBbackend/CaixaDbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.caixaDBbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaixaDbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaixaDbBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/caixaDBbackend/CaixaDbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caixaDBbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaixaDbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
