a32b12c694319660866809dc8740e1f90c03dfe5
yandv <allanmxr@gmail.com>
2023-11-07 13:05:51 -0300

    feat: implementado o relacionamento de ItemSell e Cashier no Sale

--- src/main/java/com/example/patocheckout/entities/Sale.java
+++ src/main/java/com/example/patocheckout/entities/Sale.java
@@ -1,16 +1,18 @@
 package com.example.patocheckout.entities;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.data.annotation.CreatedDate;
-
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,8 +23,20 @@ public class Sale {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    /**
+     * Relacionamento 1 para muitos com a entidade ItemSell
+     */
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = { CascadeType.ALL })
+    @JoinColumn(name = "sale_id")
     private List<ItemSell> items;
 
+    /**
+     * Relacionamento muitos para 1 com entidade Cashier
+     */
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "cashier_id")
     private Cashier cashier;
 
     private LocalDate saleDate = LocalDate.now();
