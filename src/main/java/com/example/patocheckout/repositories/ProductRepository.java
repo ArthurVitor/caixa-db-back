e13ca3b5e71855c29f5d1fd919561cbb2ffa3c88
nicofercavv-dev <nicoleferreira281@gmail.com>
2023-11-16 12:13:19 -0300

    feat(/controller/ProductController): implementa os endpoints de Product - @nicofercavv-dev

--- src/main/java/com/example/patocheckout/repositories/ProductRepository.java
+++ src/main/java/com/example/patocheckout/repositories/ProductRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.patocheckout.entities.Product;
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByBarcode(String barcode);
     
 }
